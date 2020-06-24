9f8fe26c8981725dca0c4aa3763759b4b8feebd0
phufbq <philipbroadey@gmail.com>
2020-06-24 16:12:20 +0100

    Implement square and cube

--- queryProcessor.js
+++ queryProcessor.js
@@ -22,10 +22,24 @@ function processQuery(query) {
 	else if (query.includes("Bond")) {
 		return 1962;
 	}
+	else if (query.includes("cube")) {
+		var numbers = subStrAfterChars(query, ':').split(",")
+		
+		var results = new Array();
+		for (i = 0; i < numbers.length; i++) {
+			if(sqrt(numbers[i]) && cbrt(numbers[i])) {
+				results.push(numbers[i])
+			}
+		}
+
+		return results;
+	}
 
 	else if (query.includes("largest")) {
 		var words = query.split(" ");
 		
+		words.forEach(getTheIntegers(x))
+
 	}
 	else if (query.includes("what is your name")) {
 		return "Phil"
@@ -37,6 +51,26 @@ fruits.forEach(myFunction);
 
 function getTheIntegers(item, index) {
   
+}
+
+function subStrAfterChars(str, char, pos)
+{
+  if(pos=='b')
+   return str.substring(str.indexOf(char) + 1);
+  else if(pos=='a') 
+   return str.substring(0, str.indexOf(char));
+  else
+  return str;  
+}
+
+function sqrt(x) {
+	var result = Math.sqrt(x) 
+	return (result % 1 == 0);
+}
+
+function cbrt(x) {
+	var result = Math.cbrt(x) 
+	return (result % 1 == 0);
 }
 
 module.exports = {
